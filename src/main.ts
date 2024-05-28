8592bc857378824f2b27bf9779a69a5656f32af9
Nico <akebra77@gmail.com>
2024-05-28 18:08:08 +0200

    Added Cors setup

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "*"
+    origin: "https://my-chat-r57izxo0l-nicos-projects-7661041f.vercel.app/"
   })
   await app.listen(8000);
 }
