25d06c524b9eaa07f371ff90660f7aacbc060853
Nico <akebra77@gmail.com>
2024-05-28 18:14:56 +0200

    Fixing CORS address

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "https://my-chat-seven-phi.vercel.app/"
+    origin: "https://my-chat-seven-phi.vercel.app"
   })
   await app.listen(8000);
 }
