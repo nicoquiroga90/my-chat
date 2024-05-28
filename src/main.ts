a3c68e5dcbc8ae2ecd92bc2e48bb5653bb27a28d
Nico <akebra77@gmail.com>
2024-05-28 18:59:02 +0200

    Setting cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-    app.enableCors()
+    app.enableCors({
+  origin: "https://my-chat-seven-phi.vercel.app"
+})
  
   await app.listen(8000);
 }
