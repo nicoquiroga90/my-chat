3649e1f4fb1d2f957fa7e9dc519c590d0d0c62f3
Nico <akebra77@gmail.com>
2024-05-28 18:12:59 +0200

    CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "https://my-chat-r57izxo0l-nicos-projects-7661041f.vercel.app/"
+    origin: "https://my-chat-seven-phi.vercel.app/"
   })
   await app.listen(8000);
 }
