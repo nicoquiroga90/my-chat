d54789d3f14bdb27c180198acb882c5d94833af7
Nico <akebra77@gmail.com>
2024-05-28 19:25:27 +0200

    Rolling back changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,15 +7,4 @@ import { PusherService } from './pusher.service';
   controllers: [AppController],
   providers: [PusherService],
 })
-export class AppModule {
-  configure(consumer: import('@nestjs/common').MiddlewareConsumer) {
-    consumer
-      .apply((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', 'https://my-chat-seven-phi.vercel.app');
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        next();
-      })
-      .forRoutes('*');
-  }
-}
+export class AppModule {}
