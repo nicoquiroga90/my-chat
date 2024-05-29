3b0a89c9747e69e97837e86b8f3f0b9b17d6fd97
Nico <akebra77@gmail.com>
2024-05-29 11:31:49 +0200

    adding headers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res } from '@nestjs/common'; // Importa Res para obtener acceso a la respuesta HTTP
 import { PusherService } from './pusher.service';
 
 @Controller('api')
@@ -7,10 +7,14 @@ export class AppController {
 
   @Post('messages')
   async messages(
-    @Body() body: { username: string; message: string }
+    @Body() body: { username: string; message: string },
+    @Res() res: any 
   ): Promise<any> {
     const { username, message } = body;
     await this.pusherService.trigger('my-chat', 'message', { username, message });
+    
+    res.setHeader('Access-Control-Allow-Origin', 'https://my-chat-seven-phi.vercel.app');
+
     return "Message sent";
   }
 }
