b82db24bb080c5b0c8ce0c36486ee00413733fa9
Nico <akebra77@gmail.com>
2024-05-29 08:21:55 +0200

    changes CORS setup

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const corsOptions: CorsOptions = {
-    origin: 'https://my-chat-seven-phi.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-  };
-  app.enableCors(corsOptions);
+  app.enableCors();
 
   await app.listen(8000);
 }
