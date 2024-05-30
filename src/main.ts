8d70cbbdfbce8bf845091c917d578d649b56cd6e
Nico <akebra77@gmail.com>
2024-05-30 07:03:45 +0200

    Rolling back cors setup

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const corsOptions: CorsOptions = {
-    origin: 'https://my-chat-seven-phi.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization',
-    preflightContinue: false,
-    optionsSuccessStatus: 204
-  };
+  app.enableCors();
 
-  app.enableCors(corsOptions);
-
-  await app.listen(3000);
+  await app.listen(8000);
 }
 bootstrap();
