aa2838f5779002906e88406c936cd335ab20d1f4
Nico <akebra77@gmail.com>
2024-05-30 07:12:28 +0200

    Added api status check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get } from '@nestjs/common';
 import { PusherService } from './pusher.service';
 
 @Controller('api')
@@ -12,5 +12,10 @@ export class AppController {
     const { username, message } = body;
     await this.pusherService.trigger('my-chat', 'message', { username, message });
     return "Message sent";
+  }
+
+  @Get('status')
+  getStatus(): string {
+    return "Api online";
   }
 }
