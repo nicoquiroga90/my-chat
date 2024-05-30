301cfc06051ec0f0e56978780ac516842c22c0c1
Nico <akebra77@gmail.com>
2024-05-30 06:42:32 +0200

    fixing cors error

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,4 @@
-import { Body, Controller, Get, Post, Res } from '@nestjs/common';
-import { Response } from 'express';
+import { Body, Controller, Post } from '@nestjs/common';
 import { PusherService } from './pusher.service';
 
 @Controller('api')
@@ -13,15 +12,5 @@ export class AppController {
     const { username, message } = body;
     await this.pusherService.trigger('my-chat', 'message', { username, message });
     return "Message sent";
-  }
-
-  @Get('messages')
-  options(@Res() res: Response) {
-    res.set({
-      'Access-Control-Allow-Origin': 'https://my-chat-seven-phi.vercel.app',
-      'Access-Control-Allow-Methods': 'POST',
-      'Access-Control-Allow-Headers': 'Content-Type',
-    });
-    res.status(204).end();
   }
 }
